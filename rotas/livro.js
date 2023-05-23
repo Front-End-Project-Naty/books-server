8e906d2c70ed4c822a12854596cfc295b844e88e
Naty <natyaith@yahoo.com.br>
2023-05-22 22:53:51 -0300

    add mensagem erro

--- rotas/livro.js
+++ rotas/livro.js
@@ -3,7 +3,23 @@ const { Router } = require("express")
 const router = Router()
 
 router.get('/', (req, res) => {
-    res.send("Olá!")
+    try {
+        res.send("Olá!")
+    } catch (error) {
+        res.status(500)
+    } 
+})
+
+router.post('/', (req,res) => {
+    res.send('Você fez uma requisição do tipo POST')
+})
+
+router.patch('/', (req,res) => {
+    res.send('Você fez uma requisição do tipo PATCH')
+})
+
+router.delete('/', (req,res) => {
+    res.send('Você fez uma requisição do tipo DELETE')
 })
 
 module.exports = router
