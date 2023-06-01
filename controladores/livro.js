ccff736da73a55290b1e6bb29be0e903a70451c9
Naty <natyaith@yahoo.com.br>
2023-05-31 23:31:59 -0300

    add  serviço, controladores e rotas dos favoritos

--- controladores/livro.js
+++ controladores/livro.js
@@ -43,7 +43,6 @@ function postLivro(req, res) {
         res.status(500)
         res.send(error.message)
     }
-
 }
 
 function patchLivro(req,res) {
--- rotas/livro.js
+++ rotas/livro.js
@@ -1,7 +1,6 @@
 const { Router } = require("express")
 const { getLivros, getLivro, postLivro, patchLivro, deleteLivro } = require("../controladores/livro")
 
-
 const router = Router()
 
 router.get('/', getLivros)
